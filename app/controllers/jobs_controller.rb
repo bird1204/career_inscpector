f7ecccae4f27d4cc1a922c6af0844e9139dbac79
niclin <bboyceo@hotmail.com>
2016-04-30 10:37:51 +0800

    easy search bar and api

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,13 +6,21 @@ class JobsController < ApplicationController
 
   def search
     if params[:search].present?
+      @url = "http://www.104.com.tw/i/apis/jobsearch.cfm?kws=#{params[:search]}&page=1&pgsz=2000&area=6001001000&order=2&fmt=8&cols=J,JOB,NAME,APPEAR_DATE"
     else
+      @url = "ㄏ"
     end
+    @result = JSON.parse(Net::HTTP.get_response(URI.parse(URI.encode(@url))).body)
+    @recordcount = @result['RECORDCOUNT']
+    @pagecount = @result['PAGECOUNT']
+    @page = @result['PAGE']
+    @totalpage = @result['TOTALPAGE']
+    @datas = @result['data']
   end
 
 
   private
   def url
-    "http://www.104.com.tw/i/apis/jobsearch.cfm?cat=2001001001&area=6001001001&fmt=8&cols=J,JOB,NAME"
+    "http://www.104.com.tw/i/apis/jobsearch.cfm?kws=Internet%E7%A8%8B%E5%BC%8F%E8%A8%AD%E8%A8%88%E5%B8%AB&area=6001001000&order=2&fmt=8&cols=J,JOB,NAME"
   end
 end
