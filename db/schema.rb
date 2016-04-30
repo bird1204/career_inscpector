4e3a4d38fa54100fdca25ada8fc0ace4c9536fef
niclin <bboyceo@hotmail.com>
2016-04-30 15:22:04 +0800

    add records field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430065616) do
+ActiveRecord::Schema.define(version: 20160430072106) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160430065616) do
     t.date     "record_date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "score_details", force: :cascade do |t|
