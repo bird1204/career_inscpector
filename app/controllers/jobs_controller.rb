058692666450aba29de89b2d7e5f2fbb04074845
niclin <bboyceo@hotmail.com>
2016-04-30 20:29:05 +0800

    add some alhorithm of action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
 
   def index
     @url = JSON.parse(Net::HTTP.get_response(URI.parse(URI.encode(url))).body)
-    
+
   end
 
   def search
@@ -21,12 +21,35 @@ class JobsController < ApplicationController
     end
   end
 
-  def analytics
-    #@job = params[:id]
-    #@interval = @job.record.where(status: true, record_date)
+  #算每筆工作的流動率權重
+  def score
+    #單次拉1000筆jobs作處理
+    @jobs = Job.find_each(:limit => 1000)
+    # 設定一個週期為三十天
+    @days = (30.days.ago.to_date..Date.today).map{ |date| date.strftime("%Y-%m-%d") }
 
+    #雙層迴圈就像雙層牛肉吉士堡一樣美味
+    @jobs.each do |job|
+      @days.each do |day|
+        record = Record.find(job_id: job.id, status: true, record_date: day)
+        if record.status?
+          @exist_day = @exist_day + 1
+        end
+      end
+    end
+
+    @exist_day = 0
+    @score = @exist_day / 30
   end
 
+  #找出所有職缺分數的中位數
+  def median
+    @records = Record.find_each(:limit => 1000).map{ |r| r.total}
+    array =  @records.sort!
+    elements = array.count
+    center =  elements / 2
+    elements.even? ? (array[center] + array[center+1])/2 : array[center]
+  end
 
   private
   def url
